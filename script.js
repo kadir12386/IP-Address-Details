947b69f82ad98288a52b1c1afccff76b71a0cc5f
Sangeetha <83246904+kadir12386@users.noreply.github.com>
2021-10-03 00:24:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,33 +12,33 @@ function getdata() {
 }
 
 // Creating Map for Ip Location
-function initMap(data){
-    let lati = data.latitude;
-    let long = data.longitude;
-    var map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: lati, lng: long },
-    zoom: 8,
- });
-    var marker = new google.maps.Marker({
-        position: {lat:lati, lng:long},
-        map:map
-    });
-}
+// function initMap(data){
+//     let lati = data.latitude;
+//     let long = data.longitude;
+//     var map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: lati, lng: long },
+//     zoom: 8,
+//  });
+//     var marker = new google.maps.Marker({
+//         position: {lat:lati, lng:long},
+//         map:map
+//     });
+// }
 
 // Creating and updating Html Elements
 function createElements(data){
     // console.log("in HTML",data)
-    const ip = document.querySelector(".ip");
-    ip.innerHTML ="IP Adress:"+data.ip;
-    const la = document.querySelector(".latitude");
-    la.innerHTML ="Latitude:"+data.latitude;
-    const lo = document.querySelector(".longitude");
-    lo.innerHTML ="Longitude:"+data.longitude;
-    const co = document.querySelector(".country");
-    co.innerHTML ="Country:"+data.country_name;
-    const ci = document.querySelector(".city");
-    ci.innerHTML ="City:"+data.city;
-    const zc = document.querySelector(".zipcode");
-    zc.innerHTML ="Zip-Code:"+data.zip_code;
+    const ip_add = document.querySelector(".ip");
+    ip_add.innerHTML ="IP Adress:"+data.ip;
+    const la_det = document.querySelector(".latitude");
+    la_det.innerHTML ="Latitude:"+data.latitude;
+    const lo_det = document.querySelector(".longitude");
+    lo_det.innerHTML ="Longitude:"+data.longitude;
+    const co_det = document.querySelector(".country");
+    co_det.innerHTML ="Country:"+data.country_name;
+    const ci_det = document.querySelector(".city");
+    ci_det.innerHTML ="City:"+data.city;
+    const zc_det = document.querySelector(".zipcode");
+    zc_det.innerHTML ="Zip-Code:"+data.zip_code;
 }
 
